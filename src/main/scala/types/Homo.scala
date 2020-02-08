e6ae8f74399b296adba4229b1a659827fc50e0f3
Guannan Wei <guannanwei@outlook.com>
2020-02-08 16:34:09 -0500

    cps

---
+++ src/main/scala/types/Homo.scala
@@ -0,0 +1,16 @@
+package types.crypto
+
+trait Homo {
+  /*
+  type Rep[_] // represent encrypted data
+  type D
+  type PK1 = D
+  type PK2 = D
+  type SK1 = D
+  type SK2 = D
+
+  def encrypt(pk: PK, d: D): Rep[D]
+  def decrypt(sk: SD, rd: Rep[D]): D
+  def recrypt(pk2: PK, f: Rep[SK] => Rep[D] => Rep[D], rsk1: Rep[SK], c: Rep[PK1])
+  */
+}
