59d6eae6f3253f9a334153d2e66290ba717937c7
Guannan Wei <wei220@purdue.edu>
2025-03-15 02:13:18 +0100

    clean up

--- lean/theorem_proving_in_lean4_backup/cps.lean
+++
@@ -1,9 +0,0 @@
-inductive Expr : Type
-| Lit : Nat -> Expr
-| Add : Expr -> Expr -> Expr
-
-def eval (e : Expr) (k : Nat -> Nat): Nat :=
-  match e with
-  | Expr.Lit n => n
-  | Expr.Add e1 e2 =>
-    eval e1 (fun v1 => eval e2 (fun v2 => k (v1 + v2)))
