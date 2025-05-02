82198d3e06eae92a0f13528368a3b8e3128b0b3d
Guannan Wei <guannan.wei@tufts.edu>
2025-05-02 16:50:18 +0200

    tce

---
+++ tce/evenodd_single.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef enum { fun_even, fun_odd } fun_id;
+
+int wholeprog(fun_id fun, int x) {
+  switch (fun) {
+    case fun_even: goto even;
+    case fun_odd: goto odd;
+  }
+
+  even:
+    if (x == 0) return 1;
+    x = x - 1; goto odd;
+  odd:
+    if (x == 0) return 0;
+    x = x - 1; goto even;
+}
+
+int main(int argc, char* argv[]) {
+  printf("%d\n", wholeprog(fun_even, 300000000));
+}
---
+++ tce/evenodd_trampolines.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef void* (*fun_ptr)(int);
+struct { fun_ptr fun; int arg; } resume;
+
+void* odd(int x);
+
+void* even(int x) {
+  if (x == 0) return (void*)1;
+  resume.fun = odd;
+  resume.arg = x - 1;
+  return &resume;
+}
+
+void* odd(int x) {
+  if (x == 0) return (void*)0;
+  resume.fun = even;
+  resume.arg = x - 1;
+  return &resume;
+}
+
+int main(int argc, char* argv[]) {
+  void* res = even(300000000);
+  while (res == &resume)
+    res = (resume.fun)(resume.arg);
+  printf("%d\n", (int)res);
+}
