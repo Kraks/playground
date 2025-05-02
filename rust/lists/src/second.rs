19daf42913ef03b46eb6847e0846121456cda9fc
Guannan Wei <guannan.wei@tufts.edu>
2025-05-02 16:50:19 +0200

    another version

--- rust/lists/src/second.rs
+++ rust/lists/src/second.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub struct List<T> {
     head: Link<T>,
 }
@@ -7,4 +9,49 @@ type Link<T> = Option<Box<Node<T>>>;
 struct Node<T> {
     elem: T,
     next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+    pub fn push(&mut self, elem: T) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, None) //self.head.take()
+        });
+        self.head = Some(new_node);
+    }
+    pub fn pop(&mut self) -> Option<T> {
+        mem::replace(&mut self.head, None).map(|node| {
+            self.head = node.next;
+            node.elem
+        })
+    }
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|x| { &x.elem })
+    }
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|x| { &mut x.elem })
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut cur_link = self.head.take();
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = boxed_node.next.take();
+        }
+    }
+}
+
+#[test]
+fn peek() {
+    let mut list = List::new();
+    assert_eq!(list.peek(), None);
+    assert_eq!(list.peek_mut(), None);
+    list.push(1); list.push(2); list.push(3);
+
+    assert_eq!(list.peek(), Some(&3));
+    assert_eq!(list.peek_mut(), Some(&mut 3));
 }
