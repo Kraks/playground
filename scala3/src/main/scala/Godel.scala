e10e772da647b176e0e6135afbb10bffb42ff9e3
Guannan Wei <guannan.wei@tufts.edu>
2025-05-02 16:50:19 +0200

    godel

--- scala3/src/main/scala/Godel.scala
+++ scala3/src/main/scala/Godel.scala
@@ -1,6 +1,7 @@
 package godel
 
-import scala.math.BigInt
+import math.BigInt
+import BigInt.int2bigInt
 
 object Godel {
   val fibs: LazyList[BigInt] =
@@ -18,9 +19,43 @@ object Godel {
   // If i is not a multipler of a smaller prime number, then i is prime.
   val primes: LazyList[BigInt] = BigInt(2) #:: nums3.filter { i =>
     primes.takeWhile(k => k * k <= i).forall(k => i % k > 0)
-  };
+  }
+
+  //def BigIntPower(x: BigInt, n
+
+  extension (x: BigInt)
+    def **(n: BigInt): BigInt =
+      if (n == BigInt(0)) BigInt(1)
+      else x * (x ** (n - BigInt(1)))
+
+  trait Expr {
+    def godel: BigInt
+  }
+  case class Var(x: Int) extends Expr {
+    def godel = (2 ** 0) * (3 ** x)
+  }
+  case class App(e1: Expr, e2: Expr) extends Expr {
+    def godel = (2 ** 1) * (3 ** e1.godel) * (5 ** e2.godel)
+  }
+  case class Lam(x: Int, e: Expr) extends Expr {
+    def godel = (2 ** 2) * (3 ** x) * (5 ** e.godel)
+  }
+
+  def reify(e: BigInt): Expr =
+    if ((e mod BigInt(2)) == BigInt(0)) Var((e % 2).toInt)
+    else ???
 
   def main(args: Array[String]): Unit = {
-    primes.take(20).foreach(println)
+    //primes.take(20).foreach(println)
+
+    val e = App(Lam(0, Var(0)), Lam(1, Var(1)))
+    println(e.godel)
+
+    println(Lam(0, Var(0)).godel)
+
+    println(reify(Var(0).godel))
+    println(reify(Var(5).godel))
+
+    println(3 ** 3)
   }
 }
