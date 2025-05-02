49a269ba98446bad6f3d4f44e8b9723b31fc78b3
Guannan Wei <guannan.wei@tufts.edu>
2025-05-02 16:50:21 +0200

    misc

--- lean/theorem_proving_in_lean4/prop_as_types.lean
+++ lean/theorem_proving_in_lean4/prop_as_types.lean
@@ -4,3 +4,31 @@ theorem t1 : p -> q -> p :=
   fun hp : p => fun hq : q => hp
 
 #print t1
+
+theorem t1' : p -> q -> p :=
+  fun hp : p =>
+  fun hq : q =>
+  show p from hp
+
+#print t1'
+
+theorem t1'' (hp : p) (hq : q) : p := hp
+
+#print t1''
+
+axiom hp : p
+
+theorem t2 : q → p := t1 hp
+
+axiom unsound : False
+theorem ex : 1 = 0 := False.elim unsound
+
+variable (p q : Prop)
+
+#check p → q → p ∧ q
+#check ¬p → p ↔ False
+#check p ∨ q → q ∨ p
+
+example (hp : p) (hq : q) : p ∧ q := And.intro hp hq
+
+#check fun (hp : p) (hq : q) => And.intro hp hq
